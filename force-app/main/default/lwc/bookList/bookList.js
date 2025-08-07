a46a074815cd2d6179e257f8e0f9b2b0379d3c59
Sebastian Meckovski <sebastian.meckovski@ef.com>
2025-08-07 20:21:55 +0200

    Implement updateBookRating method in BookController and add corresponding test cases in BookControllerTest; enhance bookList LWC with rating input and save functionality.

--- force-app/main/default/lwc/bookList/bookList.js
+++ force-app/main/default/lwc/bookList/bookList.js
@@ -1,6 +1,29 @@
-import { LightningElement, wire } from 'lwc';
+import { LightningElement, track, wire } from 'lwc';
 import getBooks from '@salesforce/apex/BookController.getBooks';
+import updateBookRating from '@salesforce/apex/BookController.updateBookRating';
+import { refreshApex } from '@salesforce/apex';
 
 export default class BookList extends LightningElement {
+    @track ratingEdits = {};
     @wire(getBooks) books;
+
+    handleRatingInput(event) {
+        const bookId = event.target.dataset.id;
+        this.ratingEdits = { ...this.ratingEdits, [bookId]: event.target.value };
+    }
+
+    async handleSaveRating(event) {
+        const bookId = event.target.dataset.id;
+        const newRating = parseFloat(this.ratingEdits[bookId]);
+        try {
+            await updateBookRating({ bookId, newRating });
+            this.ratingEdits = { ...this.ratingEdits, [bookId]: '' };
+            // Refresh the wire adapter
+            await refreshApex(this.books);
+        } catch (err) {
+            // Optionally handle error
+            // eslint-disable-next-line no-console
+            console.error('Error updating rating', err);
+        }
+    }
 }
